e61a32d6c44d3aac352f80c7c5af17addc424a50
Rebecca Douglass <beccadouglass@yahoo.com>
2019-07-30 22:16:59 +0000

    photo user id fix

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,14 +2,14 @@ class PhotosController < ApplicationController
     before_action :authenticate_user!
     def create
         @place = Place.find(params[:place_id])
-        @place.photos.create(photos_params.merge(user: current_user))
+        @place.photos.create (photo_params.merge(user: current_user))
         redirect_to place_path(@place)
     end
 
     private
 
     def photo_params
-        params.require(:photo).permit(:caption).require(:picture)
+        params.require(:photo).permit(:caption, :picture)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_225238) do
+ActiveRecord::Schema.define(version: 2019_07_30_220146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 2019_07_24_225238) do
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"
+    t.integer "user_id"
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "places", force: :cascade do |t|
