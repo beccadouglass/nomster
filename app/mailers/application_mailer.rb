22ca0e6b7f682f3540f36109f1a3cbcae4d246e8
Rebecca Douglass <beccadouglass@yahoo.com>
2019-07-31 20:22:17 +0000

    email config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
-  # layout 'mailer'
+    # layout 'mailer'
 
-  def comment_added
-    mail(to: "my@email.com",
-      subject: 'A comment has been added to your place')
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+
+    mail(to: @place_owner.email,
+      subject: "A comment has been added to your #{@place.name}")
   end
 end
